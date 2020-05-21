2ade5a90d4a2006fc0411503d5daa55dd49cf16d
Parker Hanson <phanson96@gmail.com>
2020-05-21 10:54:23 -0600

    Add files via upload

---
+++ Pokedex/src/model/PokemonSpecies.java
@@ -0,0 +1,18 @@
+package model;
+
+public class PokemonSpecies {
+    int id;
+    String name;
+    String speciesName;
+    String imgUrl;
+    String type1;
+    String type2;
+    String height;
+    String weight;
+    String text;
+    int hp;
+    int attack;
+    int defense;
+    int special;
+    int speed;
+}
