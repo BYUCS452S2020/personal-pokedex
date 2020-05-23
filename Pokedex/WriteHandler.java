c491309db2bf04a2a096c63f607a44179a9cacb5
Caleb Johnson <44482558+Calebdee@users.noreply.github.com>
2020-05-23 09:28:41 -0600

    Add files via upload

---
+++ Pokedex/WriteHandler.java
@@ -0,0 +1,30 @@
+package handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+
+public class WriteHandler implements HttpHandler {
+    String readString(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        InputStreamReader sr = new InputStreamReader(is);
+        char[] buf = new char[1024];
+        int len;
+        while ((len = sr.read(buf)) > 0) {
+            sb.append(buf, 0, len);
+        }
+        return sb.toString();
+    }
+
+    void writeString(String str, OutputStream os) throws IOException {
+        OutputStreamWriter writer = new OutputStreamWriter(os);
+        writer.write(str);
+        writer.flush();
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
